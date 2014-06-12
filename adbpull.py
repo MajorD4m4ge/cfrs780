108b220a73863a14267481160f453e7f214c8478
Liteman <romanokp@outlook.com>
2014-06-12 19:20:43 -0400

    Inserted correct code in adbpull.py

--- adbpull.py
+++ adbpull.py
@@ -1,17 +1,36 @@
-#update
-
 __author__ = 'Liteman'
+''' This tool requires the Android SDK - specifically adb.exe
+'''
+
+import subprocess
+import hashlib
 
 
-import hashlib 		#This imports the hashing functions
-import argparse     #http://docs.python.org/3.4/library/argparse.html
-import sys
+pathToADB = "F:\\School\\Tools\\adt-bundle-windows-x86_64-20140321\\adt-bundle-windows-x86_64-20140321\\sdk\\platform-tools"
+saveLocation = "C:\\users\\liteman\\desktop"
 
-def md5(file):
-    #define blocksize for reading file
+apps = []
+localhashlist = {}
+remotehashlist = {}
+
+#Get list of APKs on device
+listAPKs = subprocess.Popen(pathToADB + "\\adb.exe shell ls /data/app", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+#Refine list into manageable, distinct strings
+for apk in listAPKs.stdout.readlines():
+   apps.append(apk.decode("ASCII").rstrip())
+
+
+#Print Remote Hashes
+for apk in apps:
+    cmd = subprocess.Popen(pathToADB + "\\adb.exe shell md5 /data/app/" + apk, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    remotehashlist[apk] = cmd.stdout.readline().split()[0].decode("ASCII").rstrip()
+
+#Pull and Hash APKs
+for apk in apps:
+    subprocess.call(pathToADB + "\\adb.exe pull /data/app/" + apk + " " + saveLocation, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    f = open(saveLocation + "\\" + apk, 'rb')
     block = 65536
-    #open the file
-    f = open(file, 'rb')
     #read in a buffer of size block
     buf = f.read(block)
     #calculate hash
@@ -20,39 +39,26 @@ def md5(file):
     while len(buf) > 0:
         hash.update(buf)
         buf = f.read(block)
-    return hash
+    localhashlist[apk] = str(hash.hexdigest())
+    f.close()
 
-def sha(file):
-    #define blocksize for reading file
-    block = 65536
-    #open the file
-    f = open(file, 'rb')
-    #read in a buffer of size block
-    buf = f.read(block)
-    #calculate hash
-    hash = hashlib.sha1()
-    #while file has more bytes, update the hash
-    while len(buf) > 0:
-        hash.update(buf)
-        buf = f.read(block)
-    return hash
-
-def main(argv):
-    parser = argparse.ArgumentParser(description="MD5 Hash File", add_help=True)
-    parser.add_argument('-f', '--file', help='The file to be hashed.', required=False)
-    parser.add_argument('-s', '--sha1', help='Display SHA1 Hash as well', action='store_true')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.5')
-
-    args = parser.parse_args()
-    if args.file:
-        file = args.file
-        print ('MD5 Hash: ' + str(md5(file).hexdigest()))
-        print ('MD5 Hash - Upper: ' + str(md5(file).hexdigest()).upper())
-        #print (md5(file).hexdigest())
-        if args.sha1:
-            print ('SHA1 Hash: ' + str(sha(file).hexdigest()))
-            print ('SHA1 Hash - Upper: ' + str(sha(file).hexdigest()).upper())
+match = "Not Set"
+for keyLocal, valueLocal in localhashlist.items():
+    if (remotehashlist.get(keyLocal)):
+        if(valueLocal == remotehashlist[keyLocal]):
+            match = "Match"
+            print(match + "\t" + valueLocal + "\t" + remotehashlist[keyLocal] + "\t" + keyLocal)
+        else:
+            match = "Not Match"
+            print(match + "\t" + valueLocal + "\t" + remotehashlist[keyLocal] + "\t" + keyLocal)
+    else:
+        print("Error -- Local file: " + keyLocal + " was not found in the remote list")
 
 
 
-main(sys.argv[1:])
+
+
+
+
+
+
